2dfdbee38991cdf6b5112909541dc412883515c1
Deepansh Mathur <deepansh.mathur96@gmail.com>
2025-07-22 18:52:35 +0530

    Fix terraform formatting

--- main.tf
+++ main.tf
@@ -50,10 +50,10 @@ resource "aws_iam_role" "lambda_exec_role" {
   name = "${var.project_name}-lambda-exec-role"
 
   assume_role_policy = jsonencode({
-    Version   = "2012-10-17",
+    Version = "2012-10-17",
     Statement = [{
-      Action    = "sts:AssumeRole",
-      Effect    = "Allow",
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
       Principal = {
         Service = "lambda.amazonaws.com"
       }
